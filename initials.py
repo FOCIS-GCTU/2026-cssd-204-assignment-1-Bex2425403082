2d7df09f5112088d3920416910203796db4bbdb8
Bex2425403082 <2425403082@live.gctu.edu.gh>
2026-05-30 21:11:45 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials
+# Assignment Number: 1
+# Name: Obed Yakpa
+# STUDENT ID: 2425403082
+# Email: 2425403082@live.gctu.edu.gh
+# Grader: Augustus
+# On my honor, Obed Yakpa, this programming assignment
+# is my own work and I have not provided code to any student
 
 def main():
-  pass #code goes here
+    # Print my initials using print function
 
+    print()
+    print("...OYB")
+    print()
+
+    print(".....OOOOOOOO..........YY........YY........BBBBBBBBBBB......")
+    print("....OOOOOOOOOO..........YY......YY.........BBBBBBBBBBBB.....")
+    print("...OO........OO..........YY....YY..........BB........BB.....")
+    print("...OO........OO...........YY..YY...........BB........BB.....")
+    print("...OO........OO............YYYY............BBBBBBBBBBB......")
+    print("...OO........OO.............YY.............BBBBBBBBBBB......")
+    print("...OO........OO.............YY.............BB........BB.....")
+    print("...OO........OO.............YY.............BB........BB.....")
+    print("....OOOOOOOOOO....**........YY........**...BBBBBBBBBBBB...**")
+    print(".....OOOOOOOO.....**........YY........**...BBBBBBBBBBB....**")
+    
+    print()
 
 
 main()
